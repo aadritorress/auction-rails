7a88c296a4cbe9fd518d7596658cb0feb1413ba1
Adriana <adriana.amaral@msn.com>
2021-05-30 14:36:38 -0500

    item-update

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :image, :name, :price, :condition, :sold, :service, :city, :user_id, :charity_id 
+  attributes :id, :image, :name, :price, :condition, :sold, :city, :user_id, :charity_id 
   has_many :bids
 end
